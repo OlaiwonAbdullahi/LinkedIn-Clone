19d8aaf07094bb0a3bfb9b74fae6666a2174957b
Abdullahi Olaiwon <olaiwonabdullahi@gmail.com>
2024-09-20 16:17:49 +0100

    delete 1 file and update 1 file

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -4,6 +4,8 @@ import postcard from "../assets/postcard.svg";
 import playbtn from "../assets/play-btn.svg";
 import puzzle from "../assets/puzzle.svg";
 import laptop from "../assets/laptop.svg";
+import briefcase from "../assets/briefcase.svg";
+import people from "../assets/people.png";
 
 const NavBar = () => {
   return (
@@ -12,28 +14,30 @@ const NavBar = () => {
         <img src={logo} className="h-20"></img>
       </div>
       <div className="flex gap-4 mt-4">
-        <div className="flex gap-14 p-1">
-          <div className="">
+        <div className="flex gap-14 p-1 font-thin text-xs">
+          <div className="items-center flex flex-col">
             <img src={postcard} alt="" className="h-7 w-7" />
             <span>Articles</span>
           </div>
-          <div className="">
+          <div className="items-center flex flex-col">
+            <img src={people} alt="" className="h-7 w-7" />
+            <span>People</span>
+          </div>
+          <div className="items-center flex flex-col">
             <img src={playbtn} alt="" className="h-7 w-7" />
-            <span>Articles</span>
+            <span>Learning</span>
           </div>
-          <div className="">
+          <div className="items-center flex flex-col">
+            <img src={briefcase} alt="" className="h-7 w-7" />
+            <span>Jobs</span>
+          </div>
+          <div className="items-center flex flex-col">
             <img src={puzzle} alt="" className="h-7 w-7" />
-            <span>Articles</span>
+            <span>Games</span>
           </div>
-          <div className="">
+          <div className="items-center flex flex-col border-l border-r p-1">
             <img src={laptop} alt="" className="h-7 w-7" />
-            <span>Articles</span>
-          </div>
-          <div className="">
-            <img src={laptop} alt="" className="h-7 w-7" />
-          </div>
-          <div className="">
-            <img src={laptop} alt="" className="h-7 w-7" />
+            <span className=" whitespace-pre">Get the App</span>
           </div>
         </div>
         <div className="flex gap-3">
