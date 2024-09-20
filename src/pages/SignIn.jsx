1117cf41ccd21db37e65aece95b5d1ccc2bab0df
Abdullahi Olaiwon <olaiwonabdullahi@gmail.com>
2024-09-20 23:00:23 +0100

    update SignIn.jsx

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -4,12 +4,12 @@ import HeroBtn from "../components/buttons/HeroBtn";
 
 const SignIn = () => {
   return (
-    <div>
+    <div className="flex flex-col items-center justify-center min-h-screen">
       <img src={Logo} alt="" />
 
-      <div className=" flex flex-col justify-center mx-auto">
+      <div className="flex flex-col justify-center items-center w-full max-w-md mx-auto">
         <h2 className="text-3xl text-textcolor">Sign In</h2>
-        <div className=" p-4">
+        <div className="p-4 w-full">
           <HeroBtn>
             <img src={Google} alt="Google logo" className="w-6 h-6" /> Continue
             with Google
@@ -19,23 +19,22 @@ const SignIn = () => {
             with Apple
           </HeroBtn>
         </div>
-        <hr className="border border-textcolor mx-4" />
+        <hr className="border border-textcolor w-full" />
         <input
           type="text"
           placeholder="Email or Phone"
-          className="border mt-4 w-2/3 border-textcolor placeholder:text-textcolor h-14 rounded-md p-2"
+          className="border mt-4 w-full border-textcolor placeholder:text-textcolor h-14 rounded-md p-2"
         />
-        <br></br>
+        <br />
         <input
           type="password"
           placeholder="Password"
-          className="border mt-4 w-2/3 border-textcolor placeholder:text-textcolor h-14 rounded-md p-2"
+          className="border mt-4 w-full border-textcolor placeholder:text-textcolor h-14 rounded-md p-2"
         />
         <br />
         <span className="text-blue font-semibold">Forgot Password?</span>
-
-        <button className="border-2 border-blue bg-blue text-white rounded-full px-3 py-1 md:px-4 md:py-2 w-full md:w-1/3 h-8 md:h-10 font-semibold text-xs md:text-base">
-          Join Now
+        <button className="border-2 border-blue bg-blue text-white rounded-full px-3 py-1 md:px-4 md:py-2 w-full h-10 font-semibold text-xs md:text-base mt-4">
+          Sign In
         </button>
       </div>
     </div>
