7c2fcf7caba819f6aa071eda316e3bb838b32c0f
Abdullahi Olaiwon <olaiwonabdullahi@gmail.com>
2024-09-20 21:50:22 +0100

    update Jobs.jsx

---
+++ src/components/Jobs.jsx
@@ -0,0 +1,15 @@
+import BButton from "./buttons/BButton";
+
+const Jobs = () => {
+  return (
+    <div>
+      <p className="text-2xl md:text-3xl mb-4">
+        Find the right job or internship for you
+      </p>
+
+      <BButton>Post A Job</BButton>
+    </div>
+  );
+};
+
+export default Jobs;
