36ff2dbb9cf955c68291ced21e6cfe6354933342
Abdullahi Olaiwon <olaiwonabdullahi@gmail.com>
2024-09-20 23:39:59 +0100

    update SignIn.jsx

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,21 +1,23 @@
 import Logo from "../assets/Logo.webp";
 import Google from "../assets/google.png";
+import apple from "../assets/apple.svg";
 import HeroBtn from "../components/buttons/HeroBtn";
 
 const SignIn = () => {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen">
-      <img src={Logo} alt="" />
-
-      <div className="flex flex-col justify-center items-center w-full max-w-md mx-auto">
-        <h2 className="text-3xl text-textcolor">Sign In</h2>
+    <div className="flex flex-col items-center min-h-screen">
+      <img src={Logo} alt="" className="self-start w-32 ml-3" />
+      <div className="flex flex-col  items-center w-full max-w-md mx-auto">
+        <h2 className="text-3xl text-textcolor self-start font-semibold">
+          Sign In
+        </h2>
         <div className="p-4 w-full">
           <HeroBtn>
             <img src={Google} alt="Google logo" className="w-6 h-6" /> Continue
             with Google
           </HeroBtn>
           <HeroBtn>
-            <img src={Google} alt="Google logo" className="w-6 h-6" /> Sign in
+            <img src={apple} alt="Google logo" className="w-6 h-6" /> Sign in
             with Apple
           </HeroBtn>
         </div>
@@ -23,19 +25,27 @@ const SignIn = () => {
         <input
           type="text"
           placeholder="Email or Phone"
+          disabled
           className="border mt-4 w-full border-textcolor placeholder:text-textcolor h-14 rounded-md p-2"
         />
         <br />
         <input
           type="password"
           placeholder="Password"
+          disabled
           className="border mt-4 w-full border-textcolor placeholder:text-textcolor h-14 rounded-md p-2"
         />
         <br />
-        <span className="text-blue font-semibold">Forgot Password?</span>
-        <button className="border-2 border-blue bg-blue text-white rounded-full px-3 py-1 md:px-4 md:py-2 w-full h-10 font-semibold text-xs md:text-base mt-4">
+        <span className="text-blue font-semibold self-start">
+          Forgot Password?
+        </span>
+        <button className="border-2 border-blue bg-blue text-white rounded-full px-3 py-1 md:px-4 md:py-2 w-full h-10 font-semibold text-base md:text-lg mt-4">
           Sign In
         </button>
+        <h2 className="mt-3">
+          New to LinkedIn?{" "}
+          <span className="text-blue font-semibold">Join now</span>
+        </h2>
       </div>
     </div>
   );
