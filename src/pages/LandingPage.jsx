dac01c7590a4aa96c95c0a0ba1ef213bad428f2b
Abdullahi Olaiwon <olaiwonabdullahi@gmail.com>
2024-09-20 13:22:48 +0100

    update LandingPage.jsx

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,7 +1,11 @@
-import React from "react";
+import NavBar from "../components/NavBar";
 
 const LandingPage = () => {
-  return <div>LandingPage</div>;
+  return (
+    <div>
+      <NavBar />
+    </div>
+  );
 };
 
 export default LandingPage;
