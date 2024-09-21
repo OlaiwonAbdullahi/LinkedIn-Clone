bc93f125841f8c52ca919a7596b2e3d728e4e764
Abdullahi Olaiwon <olaiwonabdullahi@gmail.com>
2024-09-21 10:24:08 +0100

    update MainNavBar.jsx

--- src/components/MainNavBar.jsx
+++ src/components/MainNavBar.jsx
@@ -1,8 +1,17 @@
+import LinkedIn from "../assets/LinkedIn.png";
 const MainNavBar = () => {
   return (
     <div>
       <div className="h-12 bg-white text-textcolor ">
-        <div className=""></div>
+        <div className=" flex">
+          <img src={LinkedIn} alt="" className="h-10 w-10" />
+
+          <input
+            type="text"
+            placeholder="search"
+            className="border bg-sky-200 w-56 h-8 placeholder:text-textcolor"
+          />
+        </div>
         <div className=""></div>
       </div>
     </div>
