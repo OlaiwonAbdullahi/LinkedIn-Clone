6bd9264488a8de261881ff8146b8140b8333e635
Abdullahi Olaiwon <olaiwonabdullahi@gmail.com>
2024-09-22 12:16:34 +0100

    delete 3 files and update 1 file

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,5 +1,7 @@
 import ProfilePics from "../assets/slider1.png";
+import Dots from "../assets/three-dots.svg";
 import LinkedIn from "../assets/LinkedIn.png";
+import BBotton from "./buttons/BButton.jsx";
 const Body = () => {
   return (
     <div className="flex flex-row">
@@ -22,14 +24,23 @@ const Body = () => {
         <hr />
       </div>
       <div className="basis-2/4 h-64 w-full bg-green-900"></div>
-      <div className="basis-1/4 h-64 w-full bg-red-900">
-        <div className="flex te">
-          <span className="text-right">Ad </span>
+      <div className="basis-1/4 h-72 w-full bg-white p-2 m-7 rounded-xl">
+        <div className="flex justify-end gap-2">
+          <p className="text-right">Ad </p>
+          <img src={Dots} alt="" />
         </div>
-        <p>Abdullahi ,claim your $100 in ad credit</p>
-        <div className="">
-          <img src={ProfilePics} alt="" />
-          <img src={LinkedIn} alt="" />
+        <p className="text-center font-medium mb-5">
+          Abdullahi , Unlock Your Full potential with LinkedIn Premium
+        </p>
+        <div className="flex gap-4 mx-auto justify-center mb-5">
+          <img src={ProfilePics} alt="" className="h-20 w-20" />
+          <img src={LinkedIn} alt="" className="h-20 w-20 rounded-lg" />
+        </div>
+        <p className="text-sm text-center">
+          See the list of people who Viewed your profile
+        </p>
+        <div className="flex justify-center my-3">
+          <BBotton>Try For Free</BBotton>
         </div>
       </div>
     </div>
