f519ab87c8274b9547ce0a9f5de6a4ab541cc7c6
Abdullahi Olaiwon <olaiwonabdullahi@gmail.com>
2024-09-22 15:49:01 +0100

    update Body.jsx

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -4,6 +4,10 @@ import Dots from "../assets/three-dots.svg";
 import LinkedIn from "../assets/LinkedIn.png";
 import bookmark from "../assets/bookmark.svg";
 import BBotton from "./buttons/BButton.jsx";
+import Photo from "../assets/picture.svg";
+import calendar from "../assets/calendar.svg";
+import news from "../assets/news.svg";
+import Carret from "../assets/carret.svg";
 const Body = () => {
   return (
     <div className="flex flex-row justify-between">
@@ -46,9 +50,21 @@ const Body = () => {
           <span className="font-semibold text-sm">Saved Items</span>
         </div>
       </div>
-      <div className="basis-2/4 h-full w-full bg-white p-2 m-7 rounded-xl border-gray-400"></div>
+      <div className="basis-2/4 h-full w-full ">
+        <PostForm />
+        <div className=" p-2 flex">
+          <div className=" basis-10/12  p-2">
+            <hr className="border border-textcolor" />
+          </div>
+          <div className="basis-2/12 flex whitespace-nowrap gap-2 text-xs mt-0">
+            {" "}
+            Sort By: <span className=" font-semibold">Top</span>{" "}
+            <img src={Carret} alt="" className="w-3" />
+          </div>
+        </div>
+      </div>
 
-      <div className="basis-1/4 h-72 w-full bg-white p-2 m-7 rounded-xl border-gray-400">
+      <div className="basis-1/4 h-full w-full bg-white p-3 m-7 rounded-xl border-gray-400 border">
         <div className="flex justify-end gap-2">
           <p className="text-right">Ad </p>
           <img src={Dots} alt="" />
@@ -72,3 +88,32 @@ const Body = () => {
 };
 
 export default Body;
+
+function PostForm() {
+  return (
+    <div className="h-full w-full bg-white p-2 mt-7 rounded-xl border-gray-400 border">
+      <div className=" flex gap-3">
+        <img src={ProfilePics} alt="" className="w-12 h-12 rounded-full" />
+        <input
+          type="text"
+          placeholder="Start a Post"
+          className="border border-gray-400 rounded-full w-full h-12 p-3 placeholder:font-semibold placeholder:text-textcolor"
+        />
+      </div>
+      <div className="pb-0 pt-4 mt-2 px-6 flex justify-between">
+        <div className="flex gap-2">
+          <img src={Photo} alt="" className="w-5 h-5 text-blue" />
+          <span className="text-base font-semibold ">Media</span>
+        </div>
+        <div className="flex gap-2">
+          <img src={calendar} alt="" className="w-5 h-5 text-blue" />
+          <span className="text-base font-semibold ">Event</span>
+        </div>
+        <div className="flex gap-2">
+          <img src={news} alt="" className="w-6 h-6 text-blue" />
+          <span className="text-base font-semibold ">Write Articles</span>
+        </div>
+      </div>
+    </div>
+  );
+}
