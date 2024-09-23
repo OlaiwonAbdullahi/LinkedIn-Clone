fd58fd9c31c9f68f77fec7fec2ab35c578bfa8d1
Abdullahi Olaiwon <olaiwonabdullahi@gmail.com>
2024-09-23 13:37:39 +0100

    update NavBar.jsx

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,4 +1,4 @@
-//import logo from "../assets/logo.webp";
+import logo from "../assets/logo.webp";
 import BButton from "./buttons/BButton";
 import postcard from "../assets/postcard.svg";
 import playbtn from "../assets/play-btn.svg";
@@ -10,10 +10,10 @@ import { Link } from "react-router-dom";
 
 const NavBar = () => {
   return (
-    <div className="flex justify-between items-center p-3 md:mx-20 mx-5 top-0 sticky">
+    <div className="flex justify-between items-center p-3 md:mx-20 mx-5 top-0 sticky bg-white">
       {/* Logo Section */}
       <div className="">
-        <img src={people} className="h-14 w-20 md:h-20 md:w-28" alt="Logo" />
+        <img src={logo} className="h-14 w-20 md:h-20 md:w-28" alt="Logo" />
       </div>
 
       {/* Navigation Links */}
