b775521fce1d8a2060fd877dd29abe74f1bdd2b8
Abdullahi Olaiwon <olaiwonabdullahi@gmail.com>
2024-09-26 18:31:39 +0100

    update Network.jsx

--- src/pages/Network.jsx
+++ src/pages/Network.jsx
@@ -2,10 +2,17 @@ import MainNavBar from "../components/MainNavBar";
 
 const Network = () => {
   return (
-    <div>
+    <div className=" bg-lightgrey">
       <MainNavBar />
-      <div className="">
-        <div className=""></div>
+      <div className="flex flex-row justify-between">
+        <div className="basis-1/4 h-full w-full bg-white md:m-7 m-0 rounded-xl border border-gray-400 md:flex md:flex-col">
+          <h2 className="p-2">Manage my network</h2>
+          <hr />
+          <h2 className="p-2">Manage my network</h2>
+          <h2 className="p-2">Manage my network</h2>
+          <h2 className="p-2">Manage my network</h2>
+        </div>
+        <div className="basis-3/4 h-full w-full bg-white md:m-7 m-0 rounded-xl border border-gray-400 md:flex md:flex-col"></div>
       </div>
     </div>
   );
